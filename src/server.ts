645902b9aa46df866e7ba12e63b58b386e05ffe3
Walisson Silva <walissonsilva.dev@gmail.com>
2023-01-22 00:29:44 -0300

    feat: add cors using @fastify/cros

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
 
 const app = fastify();
 const prisma = new PrismaClient();
+
+app.register(cors);
 
 app.get("/", async (req, res) => {
   const habits = await prisma.habit.findMany();
